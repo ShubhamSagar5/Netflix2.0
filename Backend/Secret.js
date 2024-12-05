0aadb66e5be6c945642c6f129f5f1a2900d64dde
shubham <shubhamsagar628@gmail.com>
2024-12-05 12:10:42 +0530

    Done

---
+++ Backend/Secret.js
@@ -0,0 +1,9 @@
+const PORT = 4000 
+const MONGO_URL = "mongodb+srv://s60889355:6kO3skMaRco7DaZs@cluster0.zwttd.mongodb.net/Netflix"
+const JET_SECRET = "NeTFLix2.0" 
+
+module.exports = {
+    PORT,
+    MONGO_URL,
+    JET_SECRET
+}
